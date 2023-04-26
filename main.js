6ebb11992f8cf947075f998867098b11f9e889fc
Moffat <moffat.muga@gmail.com>
2023-04-27 00:20:48 +0300

    added fetch api

--- main.js
+++ main.js
@@ -15,3 +15,44 @@ const addToCartButtons = document.querySelectorAll(".add");
           button.textContent = "Added to cart!";
         });
       });
+
+fetch("https://api.sampleapis.com/coffee/hot").then((data) =>{
+      //console.log(data);
+      return data.json();
+}).then((completedata) => {
+  //console.log(completedata);
+  let data1 = "";
+  completedata.map((values) =>{
+    data1+= `<div class="box">
+    <img src=${values.image}alt="">
+    <h3 class = "title">${values.title}</h3>
+    <div class="content">
+        <span class="price">ksh.250</span>
+        <a href="#" class="add">Add to cart</a>
+    </div>
+</div>`
+  })
+  document.getElementById("cards").innerHTML = data1;
+}).catch((err) => {
+  console.log(err);
+})
+
+document.querySelector('.review-button').addEventListener('click', getPosts)
+
+/*function getReviews() {
+  fetch('https://api.sampleapis.com/coffee/iced')
+  .then((res) => res.json())
+  .then((data) => {
+    let output = <h2>reviews</h2>;
+    data.forEach(function(reviews) {
+      output += `
+        <div>
+            <h3>${value.title}</h3>
+            <p>${value.description}</p>
+        </div>
+      `;
+    }
+  );
+  document.getElementById('output').innerHTML=output;
+});
+}*/
